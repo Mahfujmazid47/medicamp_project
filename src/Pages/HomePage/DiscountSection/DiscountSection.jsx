ebe9809419db9e8ac7d6712e140c910c02fe7252
Mahfujmazid47 <mahfujmazid47@gmail.com>
2025-09-16 17:05:31 +0600

    change the bg color of sale section

--- src/Pages/HomePage/DiscountSection/DiscountSection.jsx
+++ src/Pages/HomePage/DiscountSection/DiscountSection.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 
 const DiscountSection = () => {
     return (
-        <section data-aos='fade-up' className="w-10/12 mx-auto bg-gradient-to-br from-primary/30 to-secondary/10 py-10 px-4 text-center rounded-xl shadow-md mt-8">
+        <section data-aos='fade-up' className="w-10/12 mx-auto bg-gradient-to-br from-primary/40 to-secondary/20 py-10 px-4 text-center rounded-xl shadow-md mt-8">
             <div className="max-w-xl mx-auto">
                 {/* Heading */}
                 <div className="flex justify-center items-center gap-2 mb-3">
